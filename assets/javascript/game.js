b879661908dfe65926fb40a183dfbe3791e41206
Gecko Uno <dominica.patterson@gmail.com>
2020-01-19 23:48:26 -0800

    updating images & JS file with initial pseudocode notes

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+/*Start Game
+1. Random number is chosen and displayed 
+2. Crystal buttons are each given a random number that cannot be seen
+3. Each time we click the crystal buttons the random invisible numbers are added up & displayed as your total score
+4. If we can get our total score to equal the random number we win; esle we lose
+5. When we win or lose (this is updated by an increment of 1); the game resets and starts all over again with new random numbers
+*/
+
+//Variables//
+var wins = 0
+var loses = 0
+var compGuess = "";
+
